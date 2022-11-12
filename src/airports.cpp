8c5986aca2c4094c3ad412c4fd6f6535cfce7de3
Xiaofan Li <xl121@illinois.edu>
2022-11-11 19:52:45 -0600

    changed params

--- src/airports.cpp
+++ src/airports.cpp
@@ -7,5 +7,10 @@ using namespace std;
 
 // constructor : initializes all airports by parsing data from OpenFlights
 Airports::Airports(string name, string city, string country, string id, double latitude, double longitude) {
-
+    name_ = name;
+    city_ = city;
+    country = country_;
+    id = id_;
+    latitude_ = latitude;
+    longitude_ = longitude;
 }
